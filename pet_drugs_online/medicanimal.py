977e7af1a455547883a06a0e772b72c5ca4ca845
kdm <raziel.kdm@gmail.com>
2016-11-24 23:21:19 +0300

    fixed: can't crawle pagination; fixed brand scraping;

--- pet_drugs_online/medicanimal.py
+++ pet_drugs_online/medicanimal.py
@@ -15,38 +15,38 @@ from scrapy.spider import BaseSpider
 from scrapy.utils.response import get_base_url
 
 HERE = os.path.abspath(os.path.dirname(__file__))
-SHIPPING_PRICE = 2.99;
+SHIPPING_PRICE = 2.99
+
 
 class MedicAnimalSpider(BaseSpider):
     name = 'medicanimal.com'
     allowed_domains = ['medicanimal.com']
-    start_urls = ('http:///www.medicanimal.com',)
-#    brand_list = []
+    start_urls = ('https://www.medicanimal.com/',)
 
     def parse(self, response):
         base_url = get_base_url(response)
-#        brands = response.xpath('//div[@id="productBrand"]/ul/li/form//span[@class="facet-label"]/span[@class="facet-text"]/text()').extract()
-#        brand_list += [brand.strip() for brand in brands]
-        
         main_categories = response.xpath('//nav[contains(@class, "main-navigation")]/ul/li[contains(@class, "auto")]/a/@href').extract()
         for category in main_categories:
             yield Request(urljoin(base_url, category), callback=self.parse_product_list)
 
     def parse_product_list(self, response):
         base_url = get_base_url(response)
-        products = response.css('.product-item-inner').xpath('./div/div[@class="product-image"]/a[@class="product-item-image"]/@href')
+        products = response.css('.product-item-inner').xpath('./div/div[@class="product-image"]/a[@class="product-item-image"]/@href').extract()
         for product in products:
             yield Request(urljoin(base_url, product), callback=self.parse_product)
 
         next_page = response.xpath('//ul[@class="pagination"]/li/a[@rel="next"]/@href').extract_first()
         if next_page:
-            yield Request(urljoin(base_url, next_page[0]), callback=self.parse_product_list)
+            yield Request(urljoin(base_url, next_page), callback=self.parse_product_list)
 
     def parse_product(self, response):
         name = response.xpath('//div[@class="product-details"]/h1/text()').extract_first()
         categories = response.xpath('//div[@id="breadcrumb"]/ol/li/a[not(contains(text(), "Home"))]/text()').extract()
-        
-#        identifier = response.xpath('//*[@id="product_addtocart_form"]/div[@class="no-display"]/input[@name="product"]/@value').extract_first()
+        brand = response.xpath('//head/script[@type="text/javascript"]').re(r'manufacturer: "[\w ]+"')
+        if len(brand) >= 1:
+            brand = brand[0].split(':')[-1].strip().replace('"', '')
+        else:
+            brand = ''
 
         variants = response.xpath('//ul[@id="variants-list"]/li')
         for variant in variants:
@@ -56,7 +56,7 @@ class MedicAnimalSpider(BaseSpider):
             item.add_value('name', name)
             item.add_value('name', variant_name)
             
-            variant_price = extract_price(variant.xpath('./span[@class="variant-extras"]/span[@class="variant-price "]/text()').extract_first())
+            variant_price = extract_price(variant.xpath('./span[@class="variant-extras"]/span[contains(@class, "variant-price")]/text()').extract_first())
             item.add_value('price', variant_price)
             
             item.add_value('url', response.url)
@@ -67,7 +67,7 @@ class MedicAnimalSpider(BaseSpider):
             for category in categories:
                 item.add_value('category', category)
             
-            sku = variant.xpath('.@data-variant-code').extract_first()
+            sku = variant.xpath('./@data-variant-code').extract_first()
             item.add_value('sku', sku)
             
             item.add_value('identifier', sku)
