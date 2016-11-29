f20548156215db5ea7984072fd4ebe88f9a46dc8
kdm <raziel.kdm@gmail.com>
2016-11-30 01:52:49 +0300

    second test variant, 244 errors

--- blivakker/eleven.py
+++ blivakker/eleven.py
@@ -4,19 +4,14 @@ Blivakker
 Ticket link: https://app.assembla.com/spaces/competitormonitor/tickets/5707-blivakker-%7C-cocopanda-finland-%7C-eleven/details
 """
 
+import json
 import os
-from collections import namedtuple
 from urlparse import urljoin
 
 from product_spiders.items import Product, ProductLoaderWithNameStrip as ProductLoader
-from product_spiders.phantomjs import PhantomJS
-from product_spiders.utils import extract_price
-from scrapy import signals
 from scrapy.http import Request
-from scrapy.selector import Selector
 from scrapy.spiders import BaseSpider
 from scrapy.utils.response import get_base_url
-from scrapy.xlib.pydispatch import dispatcher
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 SHIPPING_PRICE = 120
@@ -28,17 +23,10 @@ class ElevenSpider(BaseSpider):
     allowed_domains = ['eleven.se']
     start_urls = ('http://eleven.se/en/',)
 
-    def __init__(self, *args, **kwargs):
-        super(ElevenSpider, self).__init__(*args, **kwargs)
-        dispatcher.connect(self.spider_closed, signals.spider_closed)
-        self.browser = PhantomJS.create_browser()
-
-    def spider_closed(self):
-        self.browser.close()
-
     def parse(self, response):
         base_url = get_base_url(response)
-        categories = response.xpath('//div[@id="main-menu-wrapper"]/nav/ul/li/a[not(contains(text(), "BRANDS"))]/@href').extract()
+        categories = response.xpath(
+            '//div[@id="main-menu-wrapper"]/nav/ul/li/a[not(contains(text(), "BRANDS"))]/@href').extract()
         for category in categories:
             yield Request(urljoin(base_url, category), callback=self.parse_category)
 
@@ -57,74 +45,100 @@ class ElevenSpider(BaseSpider):
                 yield Request(urljoin(base_url, page), callback=self.parse_category)
 
     def parse_product(self, response):
-        base_url = get_base_url(response)
-        products = []
-        variants = response.xpath('//div[@id="pr-choose-variant"]')
-        if not variants:
-            selector = Selector(response=response)
-            item = self.create_single_product(selector)
-            products.append(item)
-        else:
-            for option_id in response.xpath('//div[@id="pr-alt"]//div[contains(@class, "pr-alt-item")]/@data-id').extract():
-                self.browser.get(response.url + '#{0}'.format(option_id))
-                variant_selector = Selector(text=self.browser.page_source)
-                # TODO: add url to variant_selector
-                item = self.create_single_product(variant_selector)
-                products.append(item)
+        products_data = {}
+        js = response.xpath('//script[contains(text(), "window.dataLayer.push")]/text()').extract_first()
+        collecting_product = False
+        extracting = ['productNumbers', 'productBarcodes', 'productPricesInclVat', 'subProductTitles']
+        for line in js.split('\n'):
+            if 'window.dataLayer.push({' in line:
+                collecting_product = True
+                continue
+            if '});' in line and collecting_product:
+                break
+            if collecting_product:
+                attr_data = [a.strip() for a in line[:-1].split(':')]
+                key = attr_data[0].replace("'", '')
+                if key in extracting:
+                    products_data[key] = eval(attr_data[1])
+                    if isinstance(products_data[key], str):
+                        products_data[key] = [products_data[key]]
 
+        products = []
+        product_options = response.xpath('//script[contains(text(), "var productItem")]/text()').extract_first()
+        # if there are product options, that means that i need to find for each option it's price, name and picture.
+        # price, name, picture and barcode is in products list
+        # id, sku and image is in options dict
+        if product_options:
+            json_clean = product_options.replace('var productItems =', '').strip()
+            options = json.loads(json_clean)
+            # here i create a product for every option. Product and option matched via SKU
+            for key, option in options.iteritems():
+                for index, sku_code in enumerate(products_data['productNumbers']):
+                    if sku_code == option[u'sProductNumber']:
+                        item = {'id': option[u'iSubProductId'],
+                                'sku': option[u'sProductNumber'],
+                                'image': option.get([u'sImgBigUrl'], ''),
+                                'name': products_data['subProductTitles'][index].encode('utf-8'),
+                                'price': products_data['productPricesInclVat'][index],
+                                'barcode': products_data['productBarcodes'][index],
+                                }
+                        if IN_STOCK_MSG in option[u'sStockXhtml']:
+                            item['stock'] = 1
+                        else:
+                            item['stock'] = 0
+
+                        products.append(item)
+
+        # if there is no product options i can leave this fields empty and take values from product page
+        else:
+            products.append({'barcode': products_data['productBarcodes'], })
+
+        # now we can create items based on our products list
         for product in products:
             item = ProductLoader(response=response, item=Product())
 
-            item.add_value('name',          product.name)
-            item.add_value('url',           product.url)
-            item.add_value('image_url',     product.image_url)
-            item.add_value('brand',         product.brand)
-            item.add_value('category',      product.category)
-            item.add_value('sku',           product.sku)
-            item.add_value('identifier',    product.id)
-            item.add_value('price',         product.price)
-            item.add_value('shipping_cost', product.shipping)
-            item.add_value('stock',         product.stock)
+            name = response.xpath('//div[@id="product"]/h1/text()').extract_first()
+            option_name = product.get('name', '')
+            item.add_value('name', name + ' ' + option_name if option_name else name)
+
+            item.add_value('url', response.url)
+
+            image_url = response.xpath('//div[@id="ImageSwitch"]//img/@src').extract_first()
+            option_image = product.get('image', '')
+            item.add_value('image_url',
+                           urljoin(response.url, option_image) if option_image else urljoin(response.url, image_url))
+
+            brand = response.xpath('//div[@id="product"]//*[@class="pr-brand-text"]/a/text()').extract_first()
+            item.add_value('brand', brand)
+
+            categories = response.xpath('//div[@id="product"]/div[@class="breadcrumbs"]//a/span/text()').extract()
+            for category in categories:
+                item.add_value('category', category)
+
+            sku = response.xpath('//form[@id="pr-form"]//input[@name="sku"]/@value').extract_first()
+            option_sku = product.get('sku', '')
+            item.add_value('sku', option_sku if option_sku else sku)
+
+            identifier = response.xpath('//form[@id="pr-form"]//input[@name="i"]/@value').extract_first()
+            option_identifier = product.get('id', '')
+            item.add_value('identifier', option_identifier if option_identifier else identifier)
+
+            price = response.xpath('//div[@id="product"]//span[contains(@class, "pr-price")]/span/text()').extract_first()
+            option_price = product.get('price', '')
+            item.add_value('price', option_price if option_price else price)
+
+            if price >= 1000:
+                item.add_value('shipping_cost', 0)
+            else:
+                item.add_value('shipping_cost', SHIPPING_PRICE)
+
+            stock = response.xpath('//div[@id="pr-info-stock"]/span[@class="availability"]/text()').extract_first()
+            option_stock = product.get('stock', '')
+            if option_stock != '':
+                item.add_value('stock', option_stock)
+            elif IN_STOCK_MSG in stock:
+                item.add_value('stock', 1)
+            else:
+                item.add_value('stock', 0)
 
             yield item.load_item()
-
-    def create_single_product(self, selector):
-
-        product = namedtuple('Item', ['name', 'url', 'image_url', 'brand', 'category', 'sku', 'id', 'price', 'shipping', 'stock'])
-
-        name = selector.xpath('//div[@id="product"]/h1/text()').extract_first()
-        product.name = name
-
-        url = selector.response.url
-        product.url = url
-
-        image_url = selector.xpath('//div[@id="ImageSwitch"]//img/@src').extract_first()
-        product.image_url = urljoin(get_base_url(selector.response), image_url)
-
-        brand = selector.xpath('//div[@id="product"]//*[@class="pr-brand-text"]/a/text()').extract_first()
-        product.brand = brand
-
-        categories = selector.xpath('//div[@id="product"]/div[@class="breadcrumbs"]//a/span/text()').extract()
-        product.category = ' '.join(categories)
-
-        sku = selector.xpath('//form[@id="pr-form"]//input[@name="sku"]/@value').extract_first()
-        product.sku = sku
-
-        identifier = selector.xpath('//form[@id="pr-form"]//input[@name="i"]/@value').extract_first()
-        product.id = identifier
-
-        price = extract_price(selector.xpath('//div[@id="product"]//span[contains(@class, "pr-price")]/span/text()').extract_first())
-        product.price = price
-
-        if price >= 1000:
-            product.shipping = 0
-        else:
-            product.shipping = SHIPPING_PRICE
-
-        stock = selector.xpath('//div[@id="pr-info-stock"]/span[@class="availability"]/text()').extract_first()
-        if stock == IN_STOCK_MSG:
-            product.stock = 1
-        else:
-            product.stock = 0
-
-        return product
